73cc5e9b1a5fd7dfc5299ccba97122319f9adc3c
Shrey Kavi <shrey.kavi@gmail.com>
2022-02-09 11:15:26 -0500

    Adding timer

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,34 @@ struct Person;
 struct Name(String);
 
 pub struct HelloPlugin;
+struct GreetTimer(Timer);
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-        .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+        .add_startup_system(add_people)
+        // .add_system(hello_world)
         .add_system(greet_people);
     }
 }
 
 fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Shrey Proctor".to_string()));
     commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-// The parameter here iterates over every Name component for entities that also 
+// The "query" parameter here iterates over every Name component for entities that also 
 // have a Person component
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+// 
+// time parameter allows us to access the Time resource
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
